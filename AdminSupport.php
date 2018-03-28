a3280691a3544dec5c4b8221f3640c444b698de8
Khoa Bui <khoaofgod@users.noreply.github.com>
2018-03-28 13:27:29 -0700

    Update AdminSupport.php

--- AdminSupport.php
+++ AdminSupport.php
@@ -984,9 +984,9 @@ class specials_AdminSupport extends specials_baseSpecials
         $key = "table_".$OPTIONS['PG_SQL']['USER'];
         $x = $this->cacheGet($key);
         if(empty($x)) {
-            $sql = 'SELECT table_name FROM information_schema.tables WHERE table_schema=? OR table_schema=? 
+            $sql = "SELECT table_name FROM information_schema.tables WHERE table_schema=? OR table_schema=? OR table_schema='commondata' 
                 GROUP BY table_name
-                order by table_name ASC';
+                order by table_name ASC";
             $x = $this->query($sql, array($OPTIONS['PG_SQL']['DBNAME'], $OPTIONS['PG_SQL']['USER']))->GetRows();
             $this->cacheSet($key,$x);
         }
